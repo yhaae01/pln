22a4bee796e11517b4f2d52e3f3f916e6ac5a4a8
yhaae01 <suryaintpermana@gmail.com>
2022-01-19 14:48:43 +0700

    Menambahkan Controller Pelanggan dan Dashboard Pelanggan

---
+++ application/controllers/Pelanggan.php
@@ -0,0 +1,35 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pelanggan extends CI_Controller 
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->session->userdata('username')) {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Silahkan login terlebih dahulu!
+            </div>');
+            redirect('auth/login_pelanggan');
+        }
+    }
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $data['title'] = 'Dashboard Pelanggan';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('pelanggan/index', $data);
+        $this->load->view('templates/footer');
+        
+    }
+
+}
+
+/* End of file pelanggan.php */
+
+?>
