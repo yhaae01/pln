6c9259ac9a808d29d9cbd11665e1352b89ed7f23
yhaae01 <suryaintpermana@gmail.com>
2022-01-23 07:38:36 +0700

    Redesign dashboard pelanggan

--- application/views/pelanggan/index.php
+++ application/views/pelanggan/index.php
@@ -4,6 +4,27 @@
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+        <div class="card">
+            <div class="card-header">
+                Selamat Datang
+            </div>
+            <div class="card-body">
+                <h6 class="card-title">Visi</h6>
+                <p class="card-text">Menjadi Perusahaan Listrik Terkemuka se-Asia Tenggara dan #1 Pilihan Pelanggan untuk Solusi Energi.</p>
+                <hr>
+                <h6>Misi</h6>
+                <ul>
+                    <li>Menjalankan bisnis kelistrikan dan bidang lain yang terkait, berorientasi pada kepuasan pelanggan, anggota perusahaan dan pemegang saham.</li>
+                    <li>Menjadikan tenaga listrik sebagai media untuk meningkatkan kualitas kehidupan masyarakat.</li>
+                    <li>Mengupayakan agar tenaga listrik menjadi pendorong kegiatan ekonomi.</li>
+                    <li>Menjalankan kegiatan usaha yang berwawasan lingkungan.</li>
+                </ul>
+                <hr>
+                <h6>Moto</h6>
+                <p>Listrik untuk Kehidupan yang Lebih Baik</p>
+            </div>
+        </div>
+
     </div>
     <!-- /.container-fluid -->
 
