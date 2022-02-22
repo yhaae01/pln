95fad9ea4fe6a7e193a2925fe41aaa43ac0749c1
yhaae01 <suryaintpermana@gmail.com>
2022-02-22 18:10:44 +0700

    Merubah tampilan pembayaran dan tagihan pada pelanggan

--- application/views/pelanggan/pembayaran/index.php
+++ application/views/pelanggan/pembayaran/index.php
@@ -4,7 +4,7 @@
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-6">
             <?= $this->session->flashdata('message'); ?>
 
                 <div class="card">
@@ -12,10 +12,6 @@
                         <div class="card-block col-md mx-auto my-2">
                             <!-- From Pembayaran -->
                             <form action="" method="post">
-                                <!-- Row -->
-                                <div class="row">
-                                    <!-- Col 1 -->
-                                    <div class="col">
                                         <div class="form-group">
                                             <input type="hidden" class="form-control" id="id_tagihan" name="id_tagihan" value="<?= $tagihan['id_tagihan'] ?>">
                                         </div>
@@ -35,40 +31,40 @@
                                             <label for="tgl_bayar">Tanggal Bayar</label>
                                             <input type="text" class="form-control" id="tgl_bayar" name="tgl_bayar" value="<?= date('d-F-Y'); ?>" readonly>
                                         </div>
-                                    </div>
-                                    <!-- End Col 1 -->
-
-                                    <!-- Col 2 -->
-                                    <div class="col mt-3">
-                                        <div class="form-group">
-                                            <label for="nama_admin">Nama Admin</label>
-                                            <input type="text" class="form-control" id="nama_admin" name="nama_admin" value="<?= $admin['nama_admin']; ?>" readonly>
-                                            <input type="hidden" class="form-control" id="id_user" name="id_user" value="<?= $admin['id_user']; ?>">
-                                        </div>
                                         
-                                        <div class="form-group">
-                                            <label for="id_pelanggan">Biaya Admin</label>
-                                            <input type="text" class="form-control" id="biaya_admin" name="biaya_admin" value="3000" readonly>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <label for="biaya_admin">Biaya Admin</label>
+                                                    <input type="hidden" class="form-control" id="nama_admin" name="nama_admin" value="<?= $admin['nama_admin']; ?>" readonly>
+                                                    <input type="hidden" class="form-control" id="id_user" name="id_user" value="<?= $admin['id_user']; ?>">
+                                                    <input type="text" class="form-control" id="biaya_admin" name="biaya_admin" value="3000" readonly>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <label for="">Biaya Tagihan</label>
+                                                    <input type="text" class="form-control" id="" name="" value="<?= $tagihan['jumlah_meter'] * $tagihan['tarif_perkwh']?>" readonly>
+                                                </div>
+                                            </div>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="id_pelanggan">Total Bayar</label>
+                                            <label for="total_bayar">Total Bayar</label>
                                             <input type="text" class="form-control" id="total_bayar" name="total_bayar" value="<?= $tagihan['jumlah_meter'] * $tagihan['tarif_perkwh'] + 3000; ?>" readonly>
                                         </div>
-                                    </div>
-                                    <!-- End Col 2 -->
-                                </div>
-                                <!-- End Row -->
-                                <div class="form-group">
-                                    <label for="nominal">Masukan Nominal Bayar</label>
-                                    <input type="text" class="form-control" id="nominal" name="nominal" placeholder="Masukan Nominal...">
-                                    <!-- Menampilkan Eror -->
-                                    <small class="form-text text-danger"><?= form_error('nominal'); ?></small>
-                                </div>
 
-                                <button type="submit" class="btn btn-primary mb-3 btn-block">
-                                    Bayar
-                                </button>
+                                        <div class="form-group">
+                                            <label for="nominal">Masukan Nominal Bayar</label>
+                                            <input type="text" class="form-control" id="nominal" name="nominal" placeholder="Masukan Nominal...">
+                                            <!-- Menampilkan Eror -->
+                                            <small class="form-text text-danger"><?= form_error('nominal'); ?></small>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary mb-3 btn-block">
+                                            Bayar
+                                        </button>
+
                             </form>
                             <!-- End Form -->
                         </div>
