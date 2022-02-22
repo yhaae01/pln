c93183b07dc7e6ae362d4e849f40fb40c11419c3
yhaae01 <suryaintpermana@gmail.com>
2022-02-22 09:48:31 +0700

    Menghapus script tidak terpakai

--- application/views/admin/penggunaan/tambah_penggunaan.php
+++ application/views/admin/penggunaan/tambah_penggunaan.php
@@ -23,12 +23,12 @@
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <label for="">Meter Awal</label>
-                                        <input type="text" name="meter_awal" id="meter_awal" class="form-control form-control-user" id="meter_awal" value="<?= set_value('meter_awal'); ?>">
+                                        <input type="text" name="meter_awal" id="meter_awal" class="form-control form-control-user" value="<?= set_value('meter_awal'); ?>">
                                         <?= form_error('meter_awal', '<small class="text-danger">', '</small>') ?>
                                 </div>
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <label for="">Meter Akhir</label>
-                                    <input type="text" name="meter_akhir" id="meter_akhir" class="form-control form-control-user" id="meter_akhir"
+                                    <input type="text" name="meter_akhir" id="meter_akhir" class="form-control form-control-user"
                                         value="<?= set_value('meter_akhir') ?>">
                                         <?= form_error('meter_akhir', '<small class="text-danger">', '</small>') ?>
                                 </div>
