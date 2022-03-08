162aec6b22db135cd9b207a2044422af14bd09ab
yhaae01 <suryaintpermana@gmail.com>
2022-03-08 13:43:38 +0700

    Merubah tampilan dashboard pelanggan

--- application/views/pelanggan/index.php
+++ application/views/pelanggan/index.php
@@ -4,24 +4,17 @@
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-        <div class="card">
-            <div class="card-header">
-                Selamat Datang
-            </div>
-            <div class="card-body">
-                <h6 class="card-title">Visi</h6>
-                <p class="card-text">Menjadi Perusahaan Listrik Terkemuka se-Asia Tenggara dan #1 Pilihan Pelanggan untuk Solusi Energi.</p>
-                <hr>
-                <h6>Misi</h6>
-                <ul>
-                    <li>Menjalankan bisnis kelistrikan dan bidang lain yang terkait, berorientasi pada kepuasan pelanggan, anggota perusahaan dan pemegang saham.</li>
-                    <li>Menjadikan tenaga listrik sebagai media untuk meningkatkan kualitas kehidupan masyarakat.</li>
-                    <li>Mengupayakan agar tenaga listrik menjadi pendorong kegiatan ekonomi.</li>
-                    <li>Menjalankan kegiatan usaha yang berwawasan lingkungan.</li>
-                </ul>
-                <hr>
-                <h6>Moto</h6>
-                <p>Listrik untuk Kehidupan yang Lebih Baik</p>
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if ($tagihan['status'] === 'Dibayar') : ?>
+                    <div class="alert alert-success" role="alert">
+                        Tagihan tidak ada atau sudah dibayar.
+                    </div>
+                <?php elseif ($tagihan['status'] === 'Belum Dibayar') : ?>
+                    <div class="alert alert-danger" role="alert">
+                        Tagihan bulan ini belum <strong>dibayar</strong>.
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
 
